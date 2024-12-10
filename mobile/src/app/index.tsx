612beaf0d0e1cfb5a2881191b6294bd072387d14
Juliana <julianafigueirac@gmail.com>
2024-12-10 19:21:15 -0300

    Ajustes do template

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+
+export default function Index(){
+
+    return (
+        <View>
+            <Text style={{backgroundColor: "white"}}>
+                Hello, React Native!
+            </Text>
+        </View>
+    )
+}
