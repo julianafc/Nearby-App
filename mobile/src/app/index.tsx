4928851fcea91918c8fba4118a4619e4fad3194e
Juliana <julianafigueirac@gmail.com>
2024-12-11 00:00:26 -0300

    primeira tela finalizada

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native"
 
 import { Welcome } from "@/components/welcome"
 import { Steps } from "@/components/steps"
+import { Button } from "@/components/button"
 
 export default function Index(){
 
@@ -9,6 +10,9 @@ export default function Index(){
         <View style={{flex: 1, padding: 40, gap: 40,}}>
             <Welcome />
             <Steps />
+            <Button>
+                <Button.Title>Começar</Button.Title>
+            </Button>
         </View>
     )
 }
